ad30742ed386cab83103187c99834101f8436816
jhoanchite <jhoanzitoxd@gmail.com>
2017-12-06 21:57:47 -0500

    05

--- web/js/app.js
+++ web/js/app.js
@@ -1,35 +1,35 @@
-
-var navBar = $ ('.navbar');
+var navBar = $('.navbar');
 navBar.append('<li class="marker"></li>');
-navBar.on("click" , "a" ,function() {
+navBar.on("click", "a", function () {
     var navTap = $(this).closest('.navbar');
     var mrkWidth = $(this).parent('li').width();
     var marker = navTap.find('.marker');
     var nx = $(".navbar").offset();
     var lx = $(this).parent('li').offset();
     var left = lx.left - nx.left;
-   navBar.find('li').removeClass('active');
-   
-   $(this).parent().addClass('active');
-    marker.removeClass("anim").css({"width" : mrkWidth , "left" : left });
-    setTimeout(function() { 
-    marker.addClass("anim")
-    },200); 
+    navBar.find('li').removeClass('active');
+
+    $(this).parent().addClass('active');
+    marker.removeClass("anim").css({
+        "width": mrkWidth,
+        "left": left
+    });
+    setTimeout(function () {
+        marker.addClass("anim")
+    }, 200);
 });
 
 
-$('.sociallink').click(function(){
-    $('.sociallinks').toggleClass('ver animated bounce');
+$('.share > a').on('click', function (e) {
+    e.preventDefault()
+    $(this).parent().find('div').toggleClass('ver animated fadeInRight');
+
+
 });
-$('.navbar-toggler').click(function(){
+$('.sociallinks').click(function () {
+    $('.sociallinks').removeClass('ver');
+});
+$('.navbar-toggler').click(function () {
     $('.navbar-collapse').toggleClass('collapse');
-    $(".collapse").slideToggle();
-    
+
 });
-
-
-
-
-
-
-
